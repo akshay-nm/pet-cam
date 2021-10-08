0555283d9e3714a97de5572c6910a49f8ebc9c81
Akshay Kumar <akshay.nm92@gmail.com>
2021-10-08 23:32:06 +0530

    initial commit

---
+++ src/lib/pet-detection/PetDetection.svelte
@@ -0,0 +1,53 @@
+<!-- Load the coco-ssd model. -->
+<script>
+	// Load TensorFlow.js. This is required to use coco-ssd model.
+	import '@tensorflow/tfjs';
+	import '@tensorflow/tfjs-backend-cpu';
+	import '@tensorflow/tfjs-backend-webgl';
+	import * as cocoSsd from '@tensorflow-models/coco-ssd';
+	import { afterUpdate } from 'svelte';
+	// Grab DOM reference to video element.
+	// const video = document.getElementById('webcamVideo');
+	// Load the model.
+
+	let videoSource = null;
+	let loading = false;
+	let initComplete = false;
+	let isDogInTheScene = false;
+	let predictions;
+
+	const obtainVideoCamera = async () => {
+		try {
+			loading = true;
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: true
+			});
+			videoSource.srcObject = stream;
+			videoSource.play();
+			loading = false;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const predictWebcam = () => {
+		cocoSsd.load().then((model) => {
+			// detect objects in the image frame.
+			model.detect(videoSource).then((pred) => {
+				//console.log('Predictions: ');
+				predictions = pred;
+			});
+		});
+		window.requestAnimationFrame(predictWebcam);
+	};
+
+	$: isDogInTheScene = predictions?.filter(
+		(prediction) => prediction.class === 'dog' && prediction.score > 0.75
+	);
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={videoSource} on:loadeddata={predictWebcam} />
+<div>is dog in the scene: {isDogInTheScene ? 'yes' : 'no'}</div>
+<div>Predictions:</div>
+<pre>{JSON.stringify(predictions, null ,2)}</pre>
+<button on:click={obtainVideoCamera}>Start</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	import PetDetection from '$lib/pet-detection/PetDetection.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<PetDetection />
